41341f6f57306111ee7b17dcbfe6e8e077783556
root <root@DESKTOP-RJD84PM.>
2024-12-29 17:39:04 +0500

    Add new game brain-progression

---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import gamePRG from "../src/brainPGRESS.js";
+
+gamePRG();
---
+++ src/brainPGRESS.js
@@ -0,0 +1,27 @@
+import { getRandomNum } from "./brainEven.js";
+import { TheMotor } from "../index.js";
+const genPRG = (length, firstElem, prgStep)=>{
+    const progression = [];
+    for (let i = 0; i < length; i += 1) {
+        progression.push(firstElem + prgStep * i);
+    }
+    return progression;
+}
+const alert = "What number is missing in the progression?";
+const genRounds = () =>{
+    const progressionLength = 10;
+    const firstElem = getRandomNum(1, 10);
+    const prgStep = getRandomNum(2,10);
+    const progression = genPRG(progressionLength, firstElem, prgStep);
+    const indexOfHide = getRandomNum(0, progressionLength - 1);
+    const hiddenElem = progression[indexOfHide];
+    progression[indexOfHide] = "..";
+    const question = progression.join(" ");
+    const rightAnswer = hiddenElem.toString();
+    return [question, rightAnswer];
+
+}
+const gamePRG = () =>{
+    TheMotor(alert, genRounds);
+}
+export default gamePRG;
