5898932fea8e67a3dc463cc4ad9ba071af8c86c9
root <root@DESKTOP-RJD84PM.>
2024-12-29 17:54:05 +0500

    add new game - brain-prime

---
+++ src/brainPrime.js
@@ -0,0 +1,25 @@
+import { getRandomNum } from "./brainEven.js";
+import { TheMotor } from "../index.js";
+const isPrime = (num)=>{
+    if (num <= 1){
+        return false;
+    };
+    for (let i = 2; i <= Math.sqrt(num); i += 1) {
+        if (num % i === 0) {
+          return false;
+        }
+    };
+    return true;
+};
+const alert = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+const genRounds = ()=>{
+    const question = getRandomNum(1, 1000)
+    const rightAnswer = isPrime(question) ? "yes": "no";
+    return [question, rightAnswer];
+
+}
+const gamePrime = ()=>{
+    TheMotor(alert, genRounds);
+}
+export default gamePrime;
+
